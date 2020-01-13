c0bf52085469b6ea8604c4afc2dfdcbe51c5a7b3
Srikavin Ramkumar <srikavinramkumar@gmail.com>
2020-01-13 08:40:04 -0500

    Update game title

--- desktop/src/me/srikavin/fbla/game/desktop/DesktopLauncher.java
+++ desktop/src/me/srikavin/fbla/game/desktop/DesktopLauncher.java
@@ -5,10 +5,11 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import me.srikavin.fbla.game.FBLAGame;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+    public static void main(String[] arg) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         config.height = 1080;
         config.width = 1920;
+        config.title = "The Quest for Success";
 //		config.foregroundFPS = 1000;
 //		config.vSyncEnabled = false;
 //		config.fullscreen = true;
